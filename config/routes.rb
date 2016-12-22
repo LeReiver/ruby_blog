733c5929db08076a66d86ee30b35437675cd37fd
Michael Le-Reiver <m.reiver@gmail.com>
2016-12-21 16:50:19 -0800

    Added scaffolding, Bootstrap,and styles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :posts
-  get 'welcome/index'
 
-  root 'welcome#index'
+  resources :posts
+
+=begin
+  get 'welcome/index'
+=end
+
+  root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
